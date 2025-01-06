7fc72359aa0270ba635840f137b498f6603c5731
manu-HP <manumcasas@gmail.com>
2025-01-06 12:52:57 +0100

    bot working completely hardcoded

--- interfaces/drvExcel.py
+++ interfaces/drvExcel.py
@@ -19,26 +19,30 @@ def openExcelFIle(path,sheetName):
     mainDataFrame = pd.read_excel(path, sheetName)
     return mainDataFrame
 
-def updateExcel(path, data ,sheetName):
-    global mainDataFrame
+def updateExcel(path="C:\DEV\coststracker\GastosMensuales2022.xlsx", sheetName= "GastosVarios",description="TestExpense",cuantity=99,extra=""):
+    #receives info, set timestamp, and return dataframe
+    x = datetime.datetime.now()
+    data=pd.DataFrame({"Date":[x],"Description":description,"Cuantity":int(cuantity),"Extra":extra})
+
+    mainDataFrame=openExcelFIle(path,sheetName)
     # CONCAT TWO DATAFRAMES
-    mainDataFrame=pd.concat([mainDataFrame,inputFrame],ignore_index=True)
+    mainDataFrame=pd.concat([mainDataFrame,data],ignore_index=True)
     ## APPEND CSV
     writer = pd.ExcelWriter(path,engine="openpyxl", mode='a',if_sheet_exists="replace")
-    data.to_excel(writer, sheet_name=sheetName,index=False)
+    mainDataFrame.to_excel(writer, sheet_name=sheetName,index=False)
     writer.close()
     return True
 
-def infoToDataFrame(description="TestExpense",cuantity=99,extra=""):
-    #receives info, set timestamp, and return dataframe
-    x = datetime.datetime.now()
-    output=pd.DataFrame({"Date":[x],"Description":description,"Cuantity":cuantity,"Extra":extra})
-    return output
+# def infoToDataFrame(description="TestExpense",cuantity=99,extra=""):
+#     #receives info, set timestamp, and return dataframe
+#     x = datetime.datetime.now()
+#     output=pd.DataFrame({"Date":[x],"Description":description,"Cuantity":cuantity,"Extra":extra})
+#     return output
 
 # ------------------------------ sequence needed to use this module ------------------------------- #
 ## load main data frame
-mainDataFrame=openExcelFIle(csvPath)
+#mainDataFrame=openExcelFIle(csvPath)
 ## create de data Frame and update main file
-inputFrame=infoToDataFrame(Description="Kebab",Cuantity=10,Extra="")
-updateExcel(csvPath,inputFrame,sheetName)
+#inputFrame=infoToDataFrame(Description="Kebab",Cuantity=10,Extra="")
+#updateExcel(csvPath,inputFrame,sheetName)
 
---
+++ main.py
@@ -0,0 +1,105 @@
+import logging
+from telegram import (ReplyKeyboardMarkup, ReplyKeyboardRemove, Update,
+                      InlineKeyboardButton, InlineKeyboardMarkup)
+from telegram.ext import (Application, CallbackQueryHandler, CommandHandler,
+                          ContextTypes, ConversationHandler, MessageHandler, filters)
+
+from interfaces.drvExcel import updateExcel
+
+# Enable logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+
+logger = logging.getLogger(__name__)
+
+## my global var
+data_entered=[]
+
+# Define states
+TOTAL_COST, COST_DESCRIPTION, COST_EXTRA_DESC = range(3)
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    #starts the conversation 
+
+    await update.message.reply_text(
+        '<b>ingresa la plata que gastaste en el siguiente orden:\n'
+        'Costo - Descripcion - Info Extra\n'
+        'para cancelar ingresa \\cancel</b>',
+        parse_mode='HTML',
+        #reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True, resize_keyboard=True),
+    )
+
+    return TOTAL_COST
+
+
+async def total_cost(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    """Stores the user entered cost."""
+    user = update.message.from_user
+    context.user_data['total_cost'] = update.message.text
+    logger.info('the user: %s entered a total cost of : %s', user.first_name, update.message.text)
+    await update.message.reply_text(
+        f'<b>You entered {update.message.text}.\n'
+        f'What is the expense description? </b>',
+        parse_mode='HTML',
+        reply_markup=ReplyKeyboardRemove(),
+    )
+
+    return COST_DESCRIPTION
+
+
+async def cost_description(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    """Stores the cost description"""    
+    user = update.message.from_user
+    context.user_data['cost_description'] = update.message.text
+    logger.info('the user: %s entered a cost description: %s', user.first_name, update.message.text)
+    await update.message.reply_text(
+        f'<b>You entered {update.message.text}.\n'
+        f'please enter an extra description or empty space </b>',
+        parse_mode='HTML',
+        reply_markup=ReplyKeyboardRemove(),
+    )
+
+    return COST_EXTRA_DESC
+
+
+async def cost_extra_desc(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    """Asks the user to fill in the mileage or skip."""
+    context.user_data['cost_extra_desc'] = update.message.text
+    
+    global data_entered
+
+    data_entered=context.user_data
+
+    updateExcel(description=data_entered['cost_description'],cuantity=data_entered['total_cost'],extra=data_entered['cost_extra_desc'])
+
+    return ConversationHandler.END
+
+async def cancel(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    """Cancels and ends the conversation."""
+    await update.message.reply_text('Bye! Hope to talk to you again soon.', reply_markup=ReplyKeyboardRemove())
+    return ConversationHandler.END
+
+def main() -> None:
+    """Run the bot."""
+    application = Application.builder().token("5212795032:AAH32kz9IJlgoXZZFOZcQz6RfshGiWXxb_w").build()
+
+    conv_handler = ConversationHandler(
+        entry_points=[CommandHandler('start', start)],
+        states={
+            TOTAL_COST: [MessageHandler(filters.TEXT & ~filters.COMMAND, total_cost)],
+            COST_DESCRIPTION: [MessageHandler(filters.TEXT & ~filters.COMMAND, cost_description)],
+            COST_EXTRA_DESC: [MessageHandler(filters.TEXT & ~filters.COMMAND, cost_extra_desc)],
+        },
+        fallbacks=[CommandHandler('cancel', cancel)],
+    )
+
+    application.add_handler(conv_handler)
+
+    # Handle the case when a user sends /start but they're not in a conversation
+    application.add_handler(CommandHandler('start', start))
+
+    application.run_polling()
+
+
+if __name__ == '__main__':
+    main()
